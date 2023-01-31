f6e6e46a4443504de72a43acffae4616134c4016
unknown <amrebrahem226@gmail.com>
2023-01-31 15:14:14 +0200

    setup locomotive scroll and add global styles

---
+++ src/sections/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+
+const Section = styled.section`
+  position: relative;
+  min-height: 200vh;
+  overflow: hidden;
+`;
+
+const Home = () => {
+  return <Section>
+    <h1>Video</h1>
+    <h1>Logo</h1>
+    <h1>Navbar</h1>
+  </Section>;
+};
+
+export default Home;
